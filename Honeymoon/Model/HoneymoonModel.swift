de24708d9626a265a82419d16f8676a973f2d7d5
Matt Saedi <mahdiisaedii@gmail.com>
2021-05-10 16:56:36 +0430

    build a card view

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,15 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by MahDi SaeDi on 5/10/21.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place:String
+    var country:String
+    var image:String
+    
+}
