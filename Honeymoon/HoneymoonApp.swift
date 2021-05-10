5e5eac5028d6f8d85e85bbc8013f9dc375868a26
Matt Saedi <mahdiisaedii@gmail.com>
2021-05-10 16:41:37 +0430

    Create Launch Screen and add icon

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by MahDi SaeDi on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
